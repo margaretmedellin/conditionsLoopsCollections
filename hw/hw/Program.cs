b741996d2f26316602ac6610323d34b4fb571524
Margaret Medellin <Margaret.A.Medellin-1@ou.edu>
2019-01-28 11:26:55 -0600

    added review methods

--- hw/hw/Program.cs
+++ hw/hw/Program.cs
@@ -8,19 +8,19 @@ namespace hw
 {
     class Program
     {
+        double y = 0;
         static void Main(string[] args)
         {
-            double y = 0;
-            y = LineValueForY(m, x, b);
 
+           double result = LineValueForY(2, 3, 4);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
 
         static double LineValueForY (double m, double x, double b)
         {
-            double y;
-            y = m * x + b;
+            double y = m * x + b;
             return y;
-
             
         }
     }
---
+++ hw/method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class Program
+    {
+        //static double sum=0 (class level variable)
+        static void Main(string[] args)
+        {
+            double result = Addition(5.5, 2);
+            Console.WriteLine(result);
+            double val1 = 10.5;
+            double val2 = 1.5;
+            Subtraction(val1, val2);
+            //calls the method
+            Console.ReadKey();
+        }
+
+        private static void Subtraction (double val1, double val2)
+        {
+            Console.WriteLine($"{val1 - val2}");
+        }
+        static double Addition(double value1, double value2)
+        {
+            double sum = value1 + value2;
+            return sum;
+        }
+    }
+}
